93c67f61a370686e1664994779f22f143b83cf51
Prince-Hervoet <2064128441@qq.com>
2024-05-23 12:41:11 +0800

    fix: #fix some files

--- src/SomethingSeemsOff/ReactiveDev/index.ts
+++ src/SomethingSeemsOff/ReactiveDev/index.ts
@@ -1,1 +1,19 @@
+function getProxyObject(obj: Object) {
+  if (!obj) return {};
+  return new Proxy(obj, {
+    get(target: any, key: string) {
+      console.log(key);
+      return target[key];
+    },
+  });
+}
+
 function executor() {}
+
+const test = getProxyObject({
+  a: 1,
+  get bar() {
+    return this.a;
+  },
+});
+console.log(test.bar);
